5452a30161424023e9621adcc06120b94b865c9e
unknown <lukamomcilovic221099@gmail.com>
2020-12-22 23:29:53 +0100

    dodao polja za unos u html fajlu dodao promenljive u js fajlu

--- hw-3/kviz.js
+++ hw-3/kviz.js
@@ -1,6 +1,12 @@
 var pitanja = new Object();
 var ime_unos = document.getElementById("ime");
 var pocni_kviz_dugme = document.getElementById("pocni_kviz");
+var forma_0 = document.getElementById("forma0");
+//forma_0.style.display = "none";
+var forma_1 = document.getElementById("forma1");
+forma_1.style.display = "none";
+var ponudjeni_odgovori_true = document.getElementById("odgovori_true");
+var ponudjeni_odgovori_false = document.getElementById("odgovori_false");
 
 fetch('kviz.json').then(function(response) {
 	return response.json();
