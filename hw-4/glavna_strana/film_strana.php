05a88a2948a070d1024401cfe63b06194006441a
unknown <lukamomcilovic221099@gmail.com>
2021-01-30 19:34:46 +0100

    dodao fajl film_strana.php na kom treba da se prikaze izabrani film

---
+++ hw-4/glavna_strana/film_strana.php
@@ -0,0 +1,55 @@
+<html>
+    <head> 
+        <title> IMDB </title>
+        <meta charset="UTF-8">
+    </head>
+
+    <body>
+        <?php
+            require_once "film_klasa.php";
+            session_start();
+            if (isset($_SESSION['id']) && isset($_POST['izabrani_film'])) {
+                $naslov_godina = explode(", ", $_POST['izabrani_film']);
+                $naslov = $naslov_godina[0];
+                $god_izdanja = (int)$naslov_godina[1];
+
+                $filmovi = $_SESSION['filmovi'];
+
+
+                $film_pronadjen = false;
+                foreach ($filmovi as $film) 
+                    if ($film->naslov === $naslov && $film->god_izdanja === $god_izdanja) {
+                        $izabrani_film = $film;
+                        $film_pronadjen = true;
+                        break;
+                    }
+                
+                if (!$film_pronadjen) {
+                    header("location: korisnik.php?error=nije_pronadjen");
+                    exit();
+                }
+            }
+            else {
+                header("location: ../pocetna_strana/login-registracija.php");
+                session_unset();
+                exit();
+            }
+        ?>
+
+        <div id="nazad">
+            <form method="post" action="korisnik.php">
+                <input type="submit" value="Nazad">
+            </form>
+        </div>
+
+        <div id="naslov"> <?php echo $izabrani_film->naslov ?> </div>
+        <div id="kraci_opis"> <?php echo $izabrani_film->kraci_opis ?> </div>
+        <div id="zanr"> <?php echo implode(", ", $izabrani_film->zanr) ?> </div>
+        <div id="reziser"> <?php echo $izabrani_film->reziser ?> </div>
+        <div id="prod_kuca"> <?php echo $izabrani_film->prod_kuca ?> </div>
+        <div id="glumci"> <?php echo $izabrani_film->glumci ?> </div>
+        <div id="god_izdanja"> <?php echo $izabrani_film->god_izdanja ?> </div>
+        <div id="slika"> <img src="<?php echo $izabrani_film->slika ?>"> </div>
+        <div id="vreme_trajanja"> <?php echo $izabrani_film->vreme_trajanja ?> </div>
+    </body>
+</html>
--- hw-4/glavna_strana/korisnik.php
+++ hw-4/glavna_strana/korisnik.php
@@ -9,9 +9,9 @@
     <input type="button" id="zanrovi_dugme" value="Izaberi žanr">
 
     <div id="svi_filmovi_kontejner">
-        <form action="" method="post">
+        <form action="film_strana.php" method="post">
             <label for="svi_filmovi_lista"> Izaberite film iz liste svih filmova: </label>
-            <input list="filmovi" name="svi_filmovi_lista" id="svi_filmovi_lista">
+            <input list="filmovi" name="izabrani_film" id="svi_filmovi_lista">
             <input type="submit" value="Potvrdi"> <br>
         </form>
     </div>
@@ -29,65 +29,65 @@
         </div>
 
         <div id="akcija">
-            <form action="" method="post">
+            <form action="film_strana.php" method="post">
                 <label for="akcija_filmovi_lista"> Akcija: </label>
-                <input list="filmovi_akcija" name="akcija_filmovi_lista" id="akcija_filmovi_lista">
+                <input list="filmovi_akcija" name="izabrani_film" id="akcija_filmovi_lista">
                 <input type="submit" value="Potvrdi">
             </form>
         </div>
 
         <div id="animirani">
-            <form action="" method="post">
+            <form action="film_strana.php" method="post">
                 <label for="animirani_filmovi_lista"> Animirani: </label>
-                <input list="filmovi_animirani" name="animirani_filmovi_lista" id="animirani_filmovi_lista">
+                <input list="filmovi_animirani" name="izabrani_film" id="animirani_filmovi_lista">
                 <input type="submit" value="Potvrdi">
             </form>
         </div>
 
         <div id="dokumentarni">
-            <form action="" method="post">
+            <form action="film_strana.php" method="post">
                 <label for="dokumentarni_filmovi_lista"> Dokumentarni: </label>
-                <input list="filmovi_dokumentarni" name="dokumentarni_filmovi_lista" id="dokumentarni_filmovi_lista">
+                <input list="filmovi_dokumentarni" name="izabrani_film" id="dokumentarni_filmovi_lista">
                 <input type="submit" value="Potvrdi">
             </form>
         </div>
 
         <div id="drama">
-            <form action="" method="post">
+            <form action="film_strana.php" method="post">
                 <label for="drama_filmovi_lista"> Drama: </label>
-                <input list="filmovi_drama" name="drama_filmovi_lista" id="drama_filmovi_lista">
+                <input list="filmovi_drama" name="izabrani_film" id="drama_filmovi_lista">
                 <input type="submit" value="Potvrdi">
             </form>
         </div>
 
         <div id="fantazija">
-            <form action="" method="post">
+            <form action="film_strana.php" method="post">
                 <label for="fantazija_filmovi_lista"> Fantazija: </label>
-                <input list="filmovi_fantazija" name="fantazija_filmovi_lista" id="fantazija_filmovi_lista">
+                <input list="filmovi_fantazija" name="izabrani_film" id="fantazija_filmovi_lista">
                 <input type="submit" value="Potvrdi">
             </form>
         </div>
 
         <div id="horor">
-            <form action="" method="post">
+            <form action="film_strana.php" method="post">
                 <label for="horor_filmovi_lista"> Horor: </label>
-                <input list="filmovi_horor" name="horor_filmovi_lista" id="horor_filmovi_lista">
+                <input list="filmovi_horor" name="izabrani_film" id="horor_filmovi_lista">
                 <input type="submit" value="Potvrdi">
             </form>
         </div>
 
         <div id="komedija">
-            <form action="" method="post">
+            <form action="film_strana.php" method="post">
                 <label for="komedija_filmovi_lista"> Komedija: </label>
-                <input list="filmovi_komedija" name="komedija_filmovi_lista" id="komedija_filmovi_lista">
+                <input list="filmovi_komedija" name="izabrani_film" id="komedija_filmovi_lista">
                 <input type="submit" value="Potvrdi">
             </form>
         </div>
 
         <div id="triler">
-            <form action="" method="post">
+            <form action="film_strana.php" method="post">
                 <label for="triler_filmovi_lista"> Triler: </label>
-                <input list="filmovi_triler" name="triler_filmovi_lista" id="triler_filmovi_lista">
+                <input list="filmovi_triler" name="izabrani_film" id="triler_filmovi_lista">
                 <input type="submit" value="Potvrdi">
             </form>
         </div>
@@ -152,7 +152,7 @@
         function ispisi_sve_filmove($filmovi) {
             echo "<datalist id='filmovi'>";
             foreach($filmovi as $film) 
-                echo "<option value='" . $film->naslov . "'>";
+                echo "<option value='" . $film->naslov . ", " . $film->god_izdanja . "'>";
             echo "</datalist>";
         }
 
@@ -161,10 +161,12 @@
             foreach($filmovi as $film) 
                 foreach ($film->zanr as $podzanr)
                     if ($podzanr === $zanr)
-                        echo "<option value='" . $film->naslov . "'>";
+                        echo "<option value='" . $film->naslov . ", " . $film->god_izdanja . "'>";
             echo "</datalist>";
         }
     ?>
+
+    <div id="nije_pronadjen"> <?php if (isset($_GET['error'])) echo "Film nije pronadjen." ?> </div>
 
     <script src="korisnik.js"> </script>
     </body>
